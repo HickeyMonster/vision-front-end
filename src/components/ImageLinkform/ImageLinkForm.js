f763a2ebef2a41462dd745e9c39f6b9edf575a80
HickeyMonster <kuzmanium@gmail.com>
2020-07-13 13:34:19 +0100

    style fix 2

--- src/components/ImageLinkform/ImageLinkForm.js
+++ src/components/ImageLinkform/ImageLinkForm.js
@@ -3,12 +3,12 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = ({onInputChange, onDetectButton}) => {
     return(
-       <div className = 'Image-form'>
+       <div>
           <p className = 'f5 black Font'>
               {'Copy and paste the URL of any image below and see vision at work!'}
           </p>
           <div className= 'center'>
-              <div className= 'form center pa4 br3 shadow-5'>
+              <div className= 'form center br3 shadow-5 Image-form'>
                     <input className= 'f5 pa1  w-70 center' type='text' onChange={onInputChange} style={{marginTop:'30px', height: '35px'}} />
                     <button className= 'ml3 tc w-25 grow link ph3 pv2 dib white bg-light-purple shadow-3' style={{marginTop:'30px'}} onClick={ onDetectButton}>Detect</button>
               </div>
